46ba27eff19cd8b6af523cf607a32ef8875c900c
Yusuke Sakamoto <sakamoto@technoface.co.jp>
2022-08-02 16:10:51 +0900

    テストソリューション作成した

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
